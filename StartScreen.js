a491dda3aab156d1656d4a7ee5016c3f80c17fa3
garajo <garrickajo@gmail.com>
2018-03-13 17:36:01 -0700

    Removed continuous rendering of card game, since there isa definitive end of cycle.

--- StartScreen.js
+++ StartScreen.js
@@ -1,24 +1,25 @@
-var startScreen={};
-startScreen.dKey=false;
-startScreen.screenText="Scene: StartScreen!"
-startScreen.demoCard=makeCard(0,0,true,false)
-startScreen.demoDeck=makeDeck(10)
+var startScreen = {};
+startScreen.dKey = false;
+startScreen.screenText = "Scene: StartScreen!"
+startScreen.demoCard = makeCard(0, 0, true, false)
+startScreen.demoDeck = makeDeck(10)
 
-startScreen.mousePressed = function(){
-    // currentScene="inGame"
+startScreen.mousePressed = function() {
+  currentScene = "inGame"
+  drawDeck(startScreen.demoDeck)
 }
-startScreen.draw = function(){
-    background('lightblue');
-    if(startScreen.dKey === true){
-        fill("black");
-        text("Well Done!",width/2,height/2+75);
+startScreen.draw = function() {
+  background('lightblue');
+  if (startScreen.dKey === true) {
+    fill("black");
+    text("Well Done!", width / 2, height / 2 + 75);
+  }
+  drawDeck(startScreen.demoDeck)
 }
-drawDeck(startScreen.demoDeck)
-}
-startScreen.keyPressed = function(){
-    console.log("hello")
-    if(key === 'D'){
-      startScreen.dKey= true;
+startScreen.keyPressed = function() {
+  console.log("hello")
+  if (key === 'D') {
+    startScreen.dKey = true;
 
-    }
+  }
 }
